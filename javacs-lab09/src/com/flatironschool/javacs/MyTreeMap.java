8df7635705990e6b6a1b33f0cca3123b436739ad
Allen Downey <downey@allendowney.com>
2016-03-24 15:26:02 -0400

    Removing solutions

--- javacs-lab09/src/com/flatironschool/javacs/MyTreeMap.java
+++ javacs-lab09/src/com/flatironschool/javacs/MyTreeMap.java
@@ -72,16 +72,7 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 		Comparable<? super K> k = (Comparable<? super K>) target;
 		
 		// the actual search
-        Node node = root;
-        while (node != null) {
-            int cmp = k.compareTo(node.key);
-            if (cmp < 0)
-                node = node.left;
-            else if (cmp > 0)
-                node = node.right;
-            else
-                return node;
-        }
+        // TODO: Fill this in.
         return null;
 	}
 
@@ -101,22 +92,6 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
 	@Override
 	public boolean containsValue(Object target) {
-		return containsValueHelper(root, target);
-	}
-
-	private boolean containsValueHelper(Node node, Object target) {
-		if (node == null) {
-			return false;
-		}
-		if (equals(target, node.value)) {
-			return true;
-		}
-		if (containsValueHelper(node.left, target)) {
-			return true;
-		}
-		if (containsValueHelper(node.right, target)) {
-			return true;
-		}
 		return false;
 	}
 
@@ -142,15 +117,8 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 	@Override
 	public Set<K> keySet() {
 		Set<K> set = new LinkedHashSet<K>();
-		addInOrder(root, set);
+        // TODO: Fill this in.
 		return set;
-	}
-
-	private void addInOrder(Node node, Set<K> set) {
-		if (node == null) return;
-		addInOrder(node.left, set);
-		set.add(node.key);
-		addInOrder(node.right, set);		
 	}
 
 	@Override
@@ -167,31 +135,8 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 	}
 
 	private V putHelper(Node node, K key, V value) {
-		@SuppressWarnings("unchecked")
-		Comparable<? super K> k = (Comparable<? super K>) key;
-		int cmp = k.compareTo(node.key);
-		
-		if (cmp < 0) {
-			if (node.left == null) {
-				node.left = new Node(key, value);
-				size++;
-				return null;
-			} else {
-				return putHelper(node.left, key, value);
-			}
-		}
-		if (cmp > 0) {
-			if (node.right == null) {
-				node.right = new Node(key, value);
-				size++;
-				return null;
-			} else {
-				return putHelper(node.right, key, value);
-			}
-		}
-		V oldValue = node.value;
-		node.value = value;
-		return oldValue;
+        // TODO: Fill this in.
+        return null;
 	}
 
 	@Override
